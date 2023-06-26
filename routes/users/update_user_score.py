3f5c67b623880924e70e2317785af322d1e90566
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2023-06-26 20:49:39 +0500

    Add files via upload

--- routes/users/update_user_score.py
+++ routes/users/update_user_score.py
@@ -1,5 +1,8 @@
 from __main__ import app, cursor, conn, request
 
+import datetime
+import random
+
 @app.route('/database/updateGJUserScore22.php', methods=['GET', 'POST'])
 async def update_user_score():
 	accId = request.form['accountID']
@@ -25,6 +28,31 @@ async def update_user_score():
 	# starsOld = cursor.fetchone()[0]
 
 	# if stars > 500: return accId, 200
+
+	# count all levels stars
+	official_levels = {
+		"stars": 190,
+		"coins": 63 # not user coins
+	}
+
+	all_stars = official_levels['stars']
+
+	cursor.execute(f'SELECT stars FROM levels')
+	data = cursor.fetchall()
+
+	for x in data:
+		all_stars = all_stars + x[0]
+	
+	if all_stars < stars:
+		cursor.execute(f"UPDATE accounts SET isBanned = 1, modLevel = 0, stars = 0 WHERE accId = {accId}")
+		conn.commit()
+
+		date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		commentId = random.randint(0, 2147483647)
+
+		cursor.execute(f"INSERT INTO posts (accId, postText, commentId, likes, isSpam, uploadDate) VALUES ({accId}, 'VGhpcyB1c2VyIHdhcyBiYW5uZWQgZnJvbSBPcGVuR0RQUyBpbnN0YW5jZS4gUmVhc29uOiBTdGFycyBvciBjb2lucyBtb3JlIHRoYW4gdGhlIG51bWJlciBvZiBhbGwgc3RhcnMvY29pbnMgZnJvbSB0aGUgc2VydmVyIChpbmNsdWRpbmcgb2ZmaWNpYWwgbGV2ZWxzKQ==', {commentId}, 0, 0, '{date}')")
+		conn.commit()
+		return accId, 200
 
 	cursor.execute(f"UPDATE accounts SET stars = {stars}, demons = {demons}, diamonds = {diamonds}, coins = {coins}, userCoins = {userCoins}, icon_cube = {icon_cube}, icon_ship = {icon_ship}, icon_ball = {icon_ball}, icon_bird = {icon_bird}, icon_dart = {icon_dart}, icon_robot = {icon_robot}, icon_glow = {icon_glow}, icon_spider = {icon_spider}, icon_explosion = {icon_explosion}, icon_type = {icon_type}, color_1 = {color_1}, color_2 = {color_2} WHERE accId = {accId}")
 	conn.commit()
